a81a3fc7bb32cc5c5e9498ca9223506ec3c2bd48
Pranshu Kashyap <pranshu.kashyap.mec19@itbhu.ac.in>
2022-04-21 00:20:12 +0530

    Add navigate onClick

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+	const navigate = useNavigate();
+
 	const [roomId, setRoomId] = useState('');
 	const [username, setUsername] = useState('');
 	const createNewRoom = (e) => {
@@ -10,7 +13,22 @@ const Home = () => {
 		const id = uuidv4();
 		console.log(id);
 		setRoomId(id);
-        toast.success('Created New Room');
+		toast.success('Created New Room');
+	};
+
+	const joinRoom = () => {
+		if (!roomId || !username) {
+			toast.error('ROOM ID and Username is required');
+			return;
+		}
+		// Redirect
+		navigate(`/editor/${roomId}`, { state: { username } });
+	};
+
+	const handleInputEnter = (e) => {
+		if (e.code === 'Enter') {
+			joinRoom();
+		}
 	};
 
 	return (
@@ -19,9 +37,11 @@ const Home = () => {
 				<img className='homepagelogo' src='/logo.png' alt='logo' />
 				<h4 className='mainlabel'>Paste Invitation ROOM ID</h4>
 				<div className='inputGroup'>
-					<input type='text' className='inputBox' placeholder='ROOM ID' value={roomId} onChange={(e) => setRoomId(e.target.value)} />
-					<input type='text' className='inputBox' placeholder='USERNAME' value={username} onChange={(e) => setUsername(e.target.value)} />
-					<button className='btn joinBtn'>Join</button>
+					<input type='text' className='inputBox' placeholder='ROOM ID' value={roomId} onKeyUp={handleInputEnter} onChange={(e) => setRoomId(e.target.value)} />
+					<input type='text' className='inputBox' placeholder='USERNAME' value={username} onKeyUp={handleInputEnter} onChange={(e) => setUsername(e.target.value)} />
+					<button className='btn joinBtn' onClick={joinRoom}>
+						Join
+					</button>
 					<span className='createInfo'>
 						If You don't have invite then create &nbsp;
 						<a href='' onClick={createNewRoom} className='createNewBtn'>
