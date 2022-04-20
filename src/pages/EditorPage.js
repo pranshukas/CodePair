526c4da4b5d39299ee243c0cfeb75da242cb1cb6
Pranshu Kashyap <pranshu.kashyap.mec19@itbhu.ac.in>
2022-04-21 00:51:38 +0530

    Add Editor Page Layout, Avatarrs

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Client from '../components/Client';
 
 const EditorPage = () => {
-	return <h1>Editor Page</h1>;
+	const [clients, setClients] = useState([
+		{ socketId: 1, username: 'Pranshu K' },
+		{ socketId: 2, username: 'Tushar S' },
+	]);
+	return (
+		<div className='mainWrap'>
+			<div className='aside'>
+				<div className='asideInner'>
+					<div className='logo'>
+						<img className='logoImage' src='/logo.png' alt='logo' />
+					</div>
+					<h3>Connected</h3>
+					<div className='clientList'>
+						{clients.map((client) => (
+							<Client key={client.socketId} username={client.username} />
+						))}
+					</div>
+				</div>
+			</div>
+			<div className='editorWrap'>Editor Goes here ...</div>
+		</div>
+	);
 };
 
 export default EditorPage;
