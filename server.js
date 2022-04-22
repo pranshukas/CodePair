df5f8a2b724154352ddc5df1000e5aa3854543d6
Pranshu Kashyap <pranshu.kashyap.mec19@itbhu.ac.in>
2022-04-23 01:09:52 +0530

    Add middleware in serverjs for production

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const ACTIONS = require('./src/Actions');
+const path = require('path');
 
 const app = express();
 
 const server = http.createServer(app);
 const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use((req, res, next) => {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const userSocketMap = {};
 
